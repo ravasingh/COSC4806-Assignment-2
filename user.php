57f09285a17a3ec7561f7589248cc745e8420ad2
ravasingh <ravasingh@algomau.ca>
2024-05-31 13:54:35 +0000

    Created user.php and created a new user table in the database and fetched it into the web.

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('database.php');
+
+class User {
+    public function get_all_users() {
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * FROM users;");
+        $statement->execute();
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    }
+?>
