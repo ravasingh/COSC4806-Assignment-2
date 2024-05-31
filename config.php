7530c6fd43bc9fcf29ff92d63b0969a83d0d471d
ravasingh <ravasingh@algomau.ca>
2024-05-31 13:19:58 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'jjg.h.filess.io');
+define('DB_USER', '4806_spendline');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', '4806_spendline');
+define('DB_PORT', '3305');
+
+?>
